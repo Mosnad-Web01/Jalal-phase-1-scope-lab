c0d77b30c4e4086ca0ea17452e30eb20a80e2e97
qusailah <qusailahj@gmail.com>
2024-08-14 14:31:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(word){
+       customerName = customerName.toUpperCase();
+
+}
+// upperCaseCustomerName();
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer= "no any thing" ;
+
+  function changeLeastFavoriteCustomer (){
+     leastFavoriteCustomer=' any thing';
+
+
+}
+
